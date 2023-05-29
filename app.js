809fa9363c4bed2fec08502faee04998b2b1a054
Szabolcs Balogh <baloghszabolcs43@gmail.com>
2023-05-29 18:23:24 +0300

    Update CSS style and JavaScript logic

--- app.js
+++ app.js
@@ -1,25 +1,51 @@
-const getElement = (id) => document.querySelector(id).value;
+let adults = document.querySelector('#adults');
+let children = document.querySelector('#children');
 let btn = document.querySelector('#buyBtn');
-btn.onclick = function () {
-  let adults = getElement('#adults');
-  let children = getElement('#children');
-  let [oneA, oneC, moreA, moreC, oneT, moreT] = ['adult', 'child', 'adults', 'children', 'ticket', 'tickets'];
-  if (adults <= 0) {
-    adults = 0;
+
+let conf = document.querySelector('#confirmation');
+let cancel = document.querySelector('.cancel');
+let title = document.querySelector('.title');
+let tickets = document.querySelector('.tickets');
+
+btn.addEventListener('click', function (event) {
+  let adValue = adults.value;
+  let chValue = children.value;
+  if (adValue <= 0) {
+    adValue = 0;
   }
-  if (children <= 0) {
-    children = 0;
+  if (chValue <= 0) {
+    chValue = 0;
   }
-  let price = calc(adults, children);
-  for (let i = 1; i <= adults; i++) {
-    console.log(`Adult Ticket #${i}`);
-  }
-  for (let i = 1; i <= children; i++) {
-    console.log(`Child Ticket #${i}`);
-  }
-  alert(`The price is \$${price}.`);
-};
+
+  let price = calc(adValue, chValue);
+  title.innerHTML = `The price is \$${price}.`;
+
+  printing(adValue, 'Adult');
+  printing(chValue, 'Child');
+  conf.style.display = 'block';
+});
+
+cancel.addEventListener('click', function (event) {
+  adults.value = '';
+  children.value = '';
+  conf.style.display = 'none';
+  let removeLis = tickets.getElementsByClassName('ticket');
+  // Convert the HTMLCollection to an Array
+  let lisArray = Array.from(removeLis);
+  lisArray.forEach((li) => {
+    li.remove();
+  });
+});
 
 function calc(adults, children) {
   return adults * 12 + children * 5;
+}
+
+function printing(nr, kind) {
+  for (let i = 1; i <= nr; i++) {
+    let li = document.createElement('li');
+    li.innerHTML = `${kind} Ticket #${i}`;
+    li.classList.add('ticket');
+    tickets.appendChild(li);
+  }
 }
